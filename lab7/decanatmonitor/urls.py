149e1813c16392567026e86a265b202dea827794
yakuzzaa <chornaya.gryazz@gmail.com>
2023-06-08 22:44:12 +0400

    add lab 7

---
+++ lab7/decanatmonitor/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('students/', views.student_list, name='student_list'),
+    path('students/create/', views.student_create, name='student_create'),
+    path('students/<int:pk>/', views.student_detail, name='student_detail'),
+    path('students/<int:pk>/update/', views.student_update, name='student_update'),
+    path('students/<int:pk>/delete/', views.student_delete, name='student_delete'),
+]
